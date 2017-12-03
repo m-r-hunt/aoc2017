ff035327279bf20b2653f204b35e4e8f5aab834f
Max Hunt <max@huntw3.com>
2017-12-03 17:05:31 +0000

    Add day 3 gif.

--- d3/main.go
+++ d3/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/m-r-hunt/mygifs"
+)
 
 const (
 	Up = iota
@@ -28,16 +31,21 @@ func addHeading(x, y, heading int) (int, int) {
 func main() {
 	// Part 1 via calculator/thought.
 
-	// Part 2
-	origin := 10
+	// Part
+	origin := 6
+	mygifs.Delay = 10
+	g := mygifs.NewGif(origin * 2 * 25, origin * 2 * 25)
+	defer g.Write("solution.gif")
 	spaces := make([][]int, origin*2)
 	for i := range spaces {
 		spaces[i] = make([]int, origin*2)
 	}
 	x, y := origin, origin
 	spaces[x][y] = 1
-	heading := Right
+	heading := Down
 	for spaces[x][y] < 277678 {
+		f := g.AddCopyFrame()
+		f.DrawText(x * 25, y * 25, fmt.Sprint(spaces[x][y]), mygifs.Black)
 		leftTurn := (heading+1)%MaxHeading
 		if nx, ny := addHeading(x, y, leftTurn); spaces[nx][ny] == 0 {
 			heading = leftTurn
@@ -53,6 +61,8 @@ func main() {
 				spaces[x][y] += spaces[x+i][y+j]
 			}
 		}
+		f.DrawText(x * 25, y * 25, fmt.Sprint(spaces[x][y]), mygifs.Red)
 	}
+	g.FreezeFrame(100)
 	fmt.Println(spaces[x][y])
 }
