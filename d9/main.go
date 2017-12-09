2848e0c2e83e176fe5768b6cf11c1f3d8ce177e8
Max Hunt <max@huntw3.com>
2017-12-09 10:03:09 +0000

    d9 done.

--- d9/main.go
+++ d9/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/m-r-hunt/mygifs"
+import (
+	"fmt"
+	"github.com/m-r-hunt/mygifs"
+)
 
 func main() {
-	lines := mygifs.JustLoadLines("input.txt")
+	l := mygifs.JustLoadLines("input.txt")[0]
+	garbo := false
+	depth := 1
+	totalScore := 0
+	garboChars := 0
+	for i := 0; i < len(l); i++ {
+		c := l[i]
+		if !garbo {
+			switch c {
+			case '{':
+				totalScore += depth
+				depth++
+			case '}':
+				depth--
+			case '<':
+				garbo = true
+			default:
+				// Do nothing
+			}
+		} else {
+			switch c {
+			case '>':
+				garbo = false
+			case '!':
+				i++
+			default:
+				garboChars++
+			}
+		}
+	}
+	fmt.Println(totalScore)
+	fmt.Println(garboChars)
 }
