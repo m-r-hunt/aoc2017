7e01ed5a08c741d7154aa8a9a356bd5a4969a293
Max Hunt <max@huntw3.com>
2017-12-12 18:42:28 +0000

    Add d11

--- d11/main.go
+++ d11/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "github.com/m-r-hunt/mygifs"
+import (
+	"github.com/m-r-hunt/mygifs"
+	"strings"
+	"fmt"
+)
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func hexDist(x, y int) int {
+	z := -x-y
+	dist := (abs(x) + abs(y) + abs(z)) / 2
+	return dist
+}
 
 func main() {
-	lines := mygifs.JustLoadLines("input.txt")
+	dirs := strings.Split(mygifs.JustLoadLines("input.txt")[0], ",")
+	x := 0
+	y := 0
+	maxDist := 0
+	for _, d := range dirs {
+		switch d {
+		case "n":
+			y -= 1
+		case "nw":
+			x -= 1
+		case "sw":
+			x -= 1
+			y += 1
+		case "s":
+			y += 1
+		case "se":
+			x += 1
+		case "ne":
+			x += 1
+			y -= 1
+		default:
+			panic(d)
+		}
+		dist := hexDist(x, y)
+		if dist > maxDist {
+			maxDist = dist
+		}
+	}
+	fmt.Println(hexDist(x, y))
+	fmt.Println(maxDist)
 }
