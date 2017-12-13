b9c67aae91e9f7bee5485dbb24677b9ee0c1683a
Max Hunt <max@huntw3.com>
2017-12-13 08:30:00 +0000

    Day 13 done.

--- d13/main.go
+++ d13/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "github.com/m-r-hunt/mygifs"
+import (
+	"github.com/m-r-hunt/mygifs"
+	"strings"
+	"strconv"
+	"fmt"
+)
 
 func main() {
 	lines := mygifs.JustLoadLines("input.txt")
+	firewalls := map[int]int{}
+	layerMax := 0
+	for _,l := range lines {
+		f := strings.Split(l, ": ")
+		layer, _ := strconv.Atoi(f[0])
+		depth, _ := strconv.Atoi(f[1])
+		firewalls[layer] = depth
+		if layer > layerMax {
+			layerMax = layer
+		}
+	}
+	fmt.Println(firewalls)
+	delay := -1
+	for {
+		delay++
+		ps := delay-1
+		severity := 0
+		caught := false
+		for ps-delay <= layerMax {
+			ps++
+			if depth, ok := firewalls[ps-delay]; ok {
+				period := (depth - 1) * 2
+				if ((ps) % period) == 0 {
+					severity += ps * depth
+					caught = true
+				}
+			}
+		}
+		if !caught {
+			fmt.Println(delay)
+			break
+		}
+		//fmt.Println(severity)
+	}
 }
