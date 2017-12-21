f2f02cc52035ced401c1d72045cdcfe566331a87
Max Hunt <max@huntw3.com>
2017-12-21 15:33:43 +0000

    Convert d5.

--- d5/main.go
+++ d5/main.go
@@ -1,49 +1,41 @@
-package main
+package d5
 
 import (
 	"fmt"
 	"github.com/m-r-hunt/mygifs"
 	"strconv"
+	"github.com/m-r-hunt/aoc2017/registry"
 )
 
-func drawJumps(f *mygifs.Frame, jumps []int) {
-	for i := 0; i < len(jumps)/2; i++ {
-		avg := (jumps[i*2] + jumps[i*2+1]) / 20
-		c := mygifs.ClampColour(mygifs.Colour(avg))
-		f.SetPixel(i, 0, c)
-		f.SetPixel(i, 1, c)
-		f.SetPixel(i, 2, c)
-		f.SetPixel(i, 3, c)
-		f.SetPixel(i, 4, c)
-	}
+func init() {
+	registry.RegisterDay(5, main)
 }
 
-func main() {
-	lines := mygifs.JustLoadLines("input.txt")
-	g := mygifs.NewGif(len(lines)/2, 20)
-	defer g.Write("solution.gif")
-	g.SetFrameskip(1000)
-	jumps := make([]int, len(lines))
-	for i := range lines {
-		n, _ := strconv.Atoi(lines[i])
-		jumps[i] = n
-	}
+func interpet(jumps []int, part2 bool) int {
 	pc := 0
 	steps := 0
 	for pc >= 0 && pc < len(jumps) {
 		steps++
 		npc := pc + jumps[pc]
-		if jumps[pc] >= 3 {
+		if part2 && jumps[pc] >= 3 {
 			jumps[pc]--
 		} else {
 			jumps[pc]++
 		}
 		pc = npc
-		f := g.AddBlankFrame()
-		if f != nil {
-			drawJumps(f, jumps)
-			f.SetPixel(pc/2, 6, mygifs.Black)
-		}
 	}
-	fmt.Println(steps)
+	return steps
+}
+
+func main() (string, string) {
+	lines := mygifs.JustLoadLines("d5/input.txt")
+	jumps := make([]int, len(lines))
+	for i := range lines {
+		n, _ := strconv.Atoi(lines[i])
+		jumps[i] = n
+	}
+	jumpsCopy := make([]int, len(lines))
+	copy(jumpsCopy, jumps)
+
+	return fmt.Sprint(interpet(jumps, false)), fmt.Sprint(interpet(jumpsCopy, true))
 }
