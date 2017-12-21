0d5d0cfee4f04405156e936c23651a9bb3c23457
Max Hunt <max@huntw3.com>
2017-12-21 13:08:23 +0000

    Refactor d21

--- d21/main.go
+++ d21/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 type rule struct {
-	input  string
-	output string
+	input  []string
+	output []string
 }
 
 func makeGrid(size int) [][]bool {
@@ -22,170 +22,82 @@ func makeGrid(size int) [][]bool {
 func applyRule(size int, i, j int, grid *[][]bool, r rule) {
 	i = i / size * (size + 1)
 	j = j / size * (size + 1)
-	fs := strings.Split(r.output, "/")
 	for m := 0; m < size+1; m++ {
 		for n := 0; n < size+1; n++ {
-			(*grid)[i+m][j+n] = fs[m][n] == '#'
+			(*grid)[i+m][j+n] = (r.output[m][n] == '#')
 		}
 	}
 }
 
 func enhance(size int, i, j int, oldGrid [][]bool, newGrid *[][]bool, rules []rule) {
+
+	transforms := []func(m, n int) (int, int){
+		func(m, n int) (int, int) { return m, n },
+		func(m, n int) (int, int) { return size - 1 - m, n },
+		func(m, n int) (int, int) { return m, size - 1 - n },
+		func(m, n int) (int, int) { return size - 1 - m, size - 1 - n },
+		func(m, n int) (int, int) { return n, m },
+		func(m, n int) (int, int) { return size - 1 - n, m },
+		func(m, n int) (int, int) { return n, size - 1 - m },
+		func(m, n int) (int, int) { return size - 1 - n, size - 1 - m },
+	}
+
+	tryTransformedRule := func(t func(m, n int) (int, int), r rule) bool {
+		for m := 0; m < size; m++ {
+			for n := 0; n < size; n++ {
+				mt, nt := t(m, n)
+				if oldGrid[i+m][j+n] != (r.input[mt][nt] == '#') {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
 	for _, r := range rules {
-		fs := strings.Split(r.input, "/")
-
-		found := true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[m][n] == '.') || (!oldGrid[i+m][j+n] && fs[m][n] == '#') {
-					found = false
-					break
-				}
+		for _, t := range transforms {
+			if tryTransformedRule(t, r) {
+				applyRule(size, i, j, newGrid, r)
+				return
 			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[size-1-m][n] == '.') || (!oldGrid[i+m][j+n] && fs[size-1-m][n] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[m][size-1-n] == '.') || (!oldGrid[i+m][j+n] && fs[m][size-1-n] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[size-1-m][size-1-n] == '.') || (!oldGrid[i+m][j+n] && fs[size-1-m][size-1-n] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[n][m] == '.') || (!oldGrid[i+m][j+n] && fs[n][m] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[n][size-1-m] == '.') || (!oldGrid[i+m][j+n] && fs[n][size-1-m] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[size-1-n][m] == '.') || (!oldGrid[i+m][j+n] && fs[size-1-n][m] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
-		}
-
-		found = true
-		for m := 0; m < size; m++ {
-			for n := 0; n < size; n++ {
-				if (oldGrid[i+m][j+n] && fs[size-1-n][size-1-m] == '.') || (!oldGrid[i+m][j+n] && fs[size-1-n][size-1-m] == '#') {
-					found = false
-					break
-				}
-			}
-		}
-		if found {
-			applyRule(size, i, j, newGrid, r)
-			return
 		}
 	}
+
 	panic("No rule matched")
 }
 
 func main() {
 	lines := mygifs.JustLoadLines("input.txt")
-	rules2 := []rule{}
-	rules3 := []rule{}
+	rules := map[int][]rule{2: []rule{}, 3: []rule{}}
 	for _, l := range lines {
 		f := strings.Fields(l)
-		if len(f[0]) == 5 {
-			rules2 = append(rules2, rule{f[0], f[2]})
+		r := rule{strings.Split(f[0], "/"), strings.Split(f[2], "/")}
+		if len(r.input) == 2 {
+			rules[2] = append(rules[2], r)
 		} else {
-			rules3 = append(rules3, rule{f[0], f[2]})
+			rules[3] = append(rules[3], r)
 		}
 	}
+
 	grid := [][]bool{
-		[]bool{false, true, false},
-		[]bool{false, false, true},
-		[]bool{true, true, true},
+		{false, true, false},
+		{false, false, true},
+		{true, true, true},
 	}
+
 	for n := 0; n < 18; n++ {
+		size := 3
 		if len(grid)%2 == 0 {
-			newGrid := makeGrid(len(grid) + len(grid)/2)
-			for i := 0; i < len(grid); i += 2 {
-				for j := 0; j < len(grid); j += 2 {
-					enhance(2, i, j, grid, &newGrid, rules2)
-				}
-			}
-			grid = newGrid
-		} else if len(grid)%3 == 0 {
-			newGrid := makeGrid(len(grid) + len(grid)/3)
-			for i := 0; i < len(grid); i += 3 {
-				for j := 0; j < len(grid); j += 3 {
-					enhance(3, i, j, grid, &newGrid, rules3)
-				}
-			}
-			grid = newGrid
-		} else {
-			panic("Bad grid size")
+			size = 2
 		}
+
+		newGrid := makeGrid(len(grid) + len(grid)/size)
+		for i := 0; i < len(grid); i += size {
+			for j := 0; j < len(grid); j += size {
+				enhance(size, i, j, grid, &newGrid, rules[size])
+			}
+		}
+		grid = newGrid
 	}
 
 	count := 0
